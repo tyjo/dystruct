06738e8dad08f5d7d67015d356d5260022d5fcb4
tjoseph <tjoseph@cs.columbia.edu>
2018-01-10 18:19:04 -0500

    update example for new interface

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 export OMP_NUM_THREADS=1
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/Users/tyjo/dystruct/boost_1_62_0/stage
 
 ./bin/dystruct --input ./supp/data/BASELINE_100GEN_10000LOCI_120D_01/samples \
                --output out \
                --npops 3 \
                --nloci 10000 \
-               --pop_size 5000 \
+               --pop-size 5000 \
+               --step-size-power -0.60 \
+               --hold-out-fraction 0.2 \
                --seed 3168
