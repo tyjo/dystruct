a19fc2c821d9b5129225e1525026acc8a62aca9f
tjoseph <tjoseph@cs.columbia.edu>
2018-05-18 11:37:02 -0400

    moved file IO to separate file; added input checks

--- src/main.cpp
+++ src/main.cpp
@@ -19,52 +19,65 @@ along with Dystruct.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
-#include <algorithm>
-#include <cstdint>
-#include <cstdlib>
 #include <cmath>
 #include <getopt.h>
 #include <iostream>
-#include <iterator>
-#include <sstream>
-#include <set>
 #include <iomanip>
-#include <map>
-#include <fstream>
 #include <string>
-#include <utility>
 #include <vector>
 
-#include "cavi.h"
+#include "svi.h"
 #include "snp_data.h"
+#include "util.h"
 
 using boost::random::mt19937;
 using boost::random::uniform_int_distribution;
 
 using std::cerr;
 using std::cout;
-using std::count;
 using std::exit;
 using std::endl;
-using std::find;
-using std::ifstream;
-using std::istringstream;
-using std::insert_iterator;
-using std::map;
-using std::pair;
-using std::set;
 using std::setprecision;
-using std::setw;
-using std::skipws;
 using std::string;
-using std::unique_copy;
 using std::vector;
 
 string VERSION = "v0.2.2";
 
-void read_snp_matrix(string fname, std_vector3<short> *snps, vector<int>& gen_sampled, int nloci);
-vector2<int> read_pop_labels(string fname, SNPData& snp_data);
-void print_help();
+
+void print_help()
+{
+    cerr << "Program: dystruct (Dynamic Structure)" << endl;
+    cerr << "Version: " << VERSION << endl;
+    cerr << "Contact: Tyler Joseph <tjoseph@cs.columbia.edu>" << endl;
+    cerr << endl;
+    cerr << "Usage:   dystruct [options]" << endl;
+    cerr << endl;
+    cerr << "Required Arguments:" << endl;
+    cerr << "\t--input FILE                " << "Genotype file path. An LOCI x INDIVIDUAL matrix of genotypes. The header" << endl
+         << "                                    is the sample time in generations. Samples must be ordered in increasing" << endl
+         << "                                    generation time. With genotypes encoder by 0, 1, 2, or 9 for missing data." << endl;
+    cerr << "\t--output STR                " << "A file prefix for output files." << endl;
+    cerr << "\t--npops INT                 " << "Number of populations." << endl;
+    cerr << "\t--loci INT                  " << "Number of loci. This should match the number of loci in the input file." << endl;
+    cerr << "\t--pop-size INT              " << "Effective population size for all populations." << endl;
+    cerr << "\t--seed INT                  " << "Random seed used to initialize variational parameters" << endl;
+    cerr << endl;
+    cerr << "Optional Arguments:" << endl;
+    cerr << "\t--hold-out-fraction DOUBLE  " << "(=0) Optional. Partitions nloci * hold_out_fraction loci into a hold out" << endl
+         << "                                    set. The hold out set contains at most one site per individual." << endl;
+    cerr << "\t--hold-out-seed INT         " << "(=28149) Optional. Random seed used to partition SNP data into hold out" << endl
+         << "                                    and training sets. Use the same seed across replicates to fix the hold" << endl
+         << "                                    out set." << endl;
+    cerr << "\t--tol DOUBLE                " << "(=1) Optional. Convergence threshold in number of loci. Programs terminates" << endl
+         << "                                    when delta < tol." << endl;
+    cerr << "\t--step-size-power DOUBLE    " << "(=-0.6) Optional. Adjusts step size for stochastic variational inference." << endl
+         << "                                    step_size = (iteration + 1)^step-size-power. Must be in [-1,-0.5)." << endl;
+    cerr << "\t--labels FILE               " << "Optional. Experimental. Population label file path for supervised analysis." << endl 
+         << "                                    Labels should be in {0,...,npops - 1}. One label per line in the same order" << endl
+         << "                                    as the input matrix. Individuals without a population assignment should be" << endl
+         << "                                    labeled by -1." << endl;
+}
+
 
 enum OPTIONS
 {
@@ -80,6 +93,7 @@ enum OPTIONS
     STEP_SIZE_POWER,
     LABELS
 };
+
 
 static struct option long_options[] =
 {
@@ -199,7 +213,6 @@ int main(int argc, char* const argv[])
     }
     
     // initialize random number generator
-    cout << "loading genotype matrix..." << endl;
     mt19937 gen(random_seed);
     std_vector3<short> *snps = new std_vector3<short>;
 
@@ -219,126 +232,11 @@ int main(int argc, char* const argv[])
     }
 
     cout << "initializing variational parameters..." << endl;
-    Cavi cavi(npop, theta_prior, pop_size, snp_data, gen, nloci, tol, step_power, labels, use_labels);
+    SVI svi(npop, theta_prior, pop_size, snp_data, gen, nloci, tol, step_power, labels, use_labels);
 
     cout << "running..." << endl;
-    cavi.run_stochastic();
-    cavi.write_results(out_file);
+    svi.run_stochastic();
+    svi.write_results(out_file);
     
     return 0;
-}
-
-
-void read_snp_matrix(string fname, std_vector3<short> *snps, vector<int>& gen_sampled, int nloci)
-{
-    ifstream input(fname);
-    if (!input.is_open()) {
-        cerr << "cannot find " << fname << endl;
-        exit(1);
-    }
-
-    string line;
-
-    // read the first line, which is the generation sampled
-    getline(input, line);
-    vector<int> generations;
-    int g;
-    istringstream iss(line);
-    while (iss >> skipws >> g) {
-        generations.push_back(g);
-    }
-    insert_iterator<vector<int> > insert_it(gen_sampled, gen_sampled.begin());
-
-    // remove duplicate sample times so we can aggregate samples by generation sampled
-    unique_copy(generations.begin(), generations.end(), insert_it);
-
-    int ct;
-    for (size_t t = 0; t < gen_sampled.size(); ++t) {
-        ct = count(generations.begin(), generations.end(), gen_sampled[t]);
-        (*snps).push_back(vector2<short>(boost::extents[ct][nloci]));
-    }
-
-    short genotype;
-    int i;
-    int l = 0;
-    int gen;
-    int index;
-    while (getline(input, line)) {
-        iss = istringstream(line);
-        i = 0;
-        map<int, int> gen_samples; // generation to number of samples
-        while (iss >> skipws >> genotype) {
-            gen = generations[i];
-            index = find(gen_sampled.begin(), gen_sampled.end(), gen) - gen_sampled.begin();
-            (*snps)[index][gen_samples[gen]][l] = genotype;
-            gen_samples[gen]++;
-            i++;
-        }
-        l++;
-        if (l == nloci)
-            break;
-    }
-    input.close();
-}
-
-
-
-vector2<int> read_pop_labels(string fname, SNPData& snp_data)
-{
-    vector2<int> labels(boost::extents[snp_data.total_time_steps()][snp_data.max_individuals()]);
-
-    ifstream input(fname);
-    if (!input.is_open()) {
-        cerr << "cannot open label file " << fname << endl;
-        exit(1);
-    }
-
-    string line;
-    istringstream iss;
-    int label;
-    for (size_t t = 0; t < snp_data.total_time_steps(); ++t) {
-        for (size_t d = 0; d < snp_data.total_individuals(t); ++d) {
-            getline(input, line);
-            iss = istringstream(line);
-            iss >> skipws >> label;
-            labels[t][d] = label;
-        }
-    }
-    return labels;
-}
-
-
-
-void print_help()
-{
-    cerr << "Program: dystruct (Dynamic Structure)" << endl;
-    cerr << "Version: " << VERSION << endl;
-    cerr << "Contact: Tyler Joseph <tjoseph@cs.columbia.edu>" << endl;
-    cerr << endl;
-    cerr << "Usage:   dystruct [options]" << endl;
-    cerr << endl;
-    cerr << "Required Arguments:" << endl;
-    cerr << "\t--input FILE                " << "Genotype file path. An LOCI x INDIVIDUAL matrix of genotypes. The header" << endl
-         << "                                    is the sample time in generations. Samples must be ordered in increasing" << endl
-         << "                                    generation time." << endl;
-    cerr << "\t--output STR                " << "A file prefix for output files." << endl;
-    cerr << "\t--npops INT                 " << "Number of populations." << endl;
-    cerr << "\t--loci INT                  " << "Number of loci. This should match the number of loci in the input file." << endl;
-    cerr << "\t--pop-size INT              " << "Effective population size for all populations." << endl;
-    cerr << "\t--seed INT                  " << "Random seed used to initialize variational parameters" << endl;
-    cerr << endl;
-    cerr << "Optional Arguments:" << endl;
-    cerr << "\t--hold-out-fraction DOUBLE  " << "(=0) Optional. Partitions nloci * hold_out_fraction loci into a hold out" << endl
-         << "                                    set. The hold out set contains at most one site per individual." << endl;
-    cerr << "\t--hold-out-seed INT         " << "(=28149) Optional. Random seed used to partition SNP data into hold out" << endl
-         << "                                    and training sets. Use the same seed across replicates to fix the hold" << endl
-         << "                                    out set." << endl;
-    cerr << "\t--tol DOUBLE                " << "(=1) Optional. Convergence threshold in number of loci. Programs terminates" << endl
-         << "                                    when delta < tol." << endl;
-    cerr << "\t--step-size-power DOUBLE    " << "(=-0.6) Optional. Adjusts step size for stochastic variational inference." << endl
-         << "                                    step_size = (iteration + 1)^step-size-power. Must be in [-1,-0.5)." << endl;
-    cerr << "\t--labels FILE               " << "Optional. Experimental. Population label file path for supervised analysis." << endl 
-         << "                                    Labels should be in {0,...,npops - 1}. One label per line in the same order" << endl
-         << "                                    as the input matrix. Individuals without a population assignment should be" << endl
-         << "                                    labeled by -1." << endl;
 }
