c6fb078f66d700b539937b5a8806b1c243719e66
tjoseph <tjoseph@cs.columbia.edu>
2018-09-13 15:42:54 -0400

    supplementary files will change for v1.0.0

--- supp/admixture/convert.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for folder in $(find ../data ! -name "simulate_data.sh" -type d);
-do
-    ./format.py $folder
-done
--- supp/data/README.md
+++
@@ -1,1 +0,0 @@
-Call simulate_data.sh and sample_size_sim.sh to regenerate the simulated data file from the paper.
