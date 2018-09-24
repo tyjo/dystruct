72e670dc48fe925bb0b0df9f2d17dd10c2fa56f7
tjoseph <tjoseph@cs.columbia.edu>
2018-09-24 17:54:39 -0400

    example script and example files

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=2
 
-./bin/dystruct --input ./supp/data/BASELINE_100GEN_10000LOCI_120D_01/samples \
+./bin/dystruct --input ./supp/example_data/samples.geno \
+               --generation-times ./supp/example_data/sample_times \
                --output out \
                --npops 3 \
                --nloci 10000 \
                --pop-size 5000 \
-               --step-size-power -0.60 \
-               --hold-out-fraction 0.2 \
-               --seed 3168
+               --seed 1145 \
+               --hold-out-fraction 0.1 \
+               --hold-out-seed 55307
